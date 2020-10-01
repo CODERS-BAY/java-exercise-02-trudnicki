b5f348e99b6da4d25616f5f94a292ed381fce34f
Lukas <lukas.wais@outlook.de>
2020-10-01 17:07:54 +0200

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,21 @@
+public class Calculator {
+
+    // global variables
+    private static final Scanner scanner = new Scanner(System.in);
+
+    /*
+        your code comes here
+     */
+
+    public static void main(String[] args) {
+        /*
+            your code comes here
+        */
+        boolean terminate = false;
+        do {
+           // do something
+        } while (!terminate);
+
+        System.out.println("Thank you for using the calculator. See you soon!");
+    }
+}
